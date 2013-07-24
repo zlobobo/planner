1c1810bf4606604f0975bdeac19acff31a0b0f61
Andrei Dzikavitski <zlobarik@gmail.com>
2013-07-24 16:57:21 +0300

    catalog

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,7 +1,14 @@
 class CatalogController < ApplicationController
+
   def index
+    category = params[:cat_id] || 'all'
     @cats = ActivityCategory.all
-    @acts = Activity.all
+    if category != 'all'
+      @acts = Activity.where('activity_category_id = ?', category)
+    else
+      @acts = Activity.all
+    end
     render layout: false
   end
+
 end
--- public/javascript/catalog.js
+++ public/javascript/catalog.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
+    $.ajaxSetup ({
+        cache: false
+    });
+
+    var path = Routes.catalog_index_path();
     $("#categories_list li a").bind("click",function(){
     	$("#categories_list li").removeClass("active_category");
     	$(this).parent().addClass("active_category");
-    });   
+        var upd_path = path + '?cat_id=' +$(this).parent().attr("id") + ' #products_container';
+        $("#products_container").load(upd_path);
+    });
 });
