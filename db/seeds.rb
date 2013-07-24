b1e94a50eedf3e1bf20a04d4124a62cdc4c9fdb8
Andrei Dzikavitski <zlobarik@gmail.com>
2013-07-24 17:25:57 +0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ActivityCategory.create([
+                { :id => 1, :name => "Hotels"},
+                { :id => 2, :name => "Restaurants"},
+                { :id => 3, :name => "Sport"},
+                { :id => 4, :name => "Disco"},
+            ], :without_protection => true )
+
+
+
+Activity.create([
+                     { :activity_category_id => 1, :name => "Hilton", :description => ""},
+                     { :activity_category_id => 1, :name => "Orbita", :description => ""},
+                     { :activity_category_id => 2, :name => "Mac", :description => ""},
+                     { :activity_category_id => 2, :name => "KFC", :description => ""},
+                     { :activity_category_id => 3, :name => "Bicycles", :description => ""},
+                     { :activity_category_id => 4, :name => "Club", :description => ""},
+                 ], :without_protection => true )
