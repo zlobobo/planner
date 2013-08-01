99de0eca636fe794c385f8a41f5c657670d2c175
Andrei Dzikavitski <zlobarik@gmail.com>
2013-08-01 18:05:53 +0300

    test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,19 @@ class HomeController < ApplicationController
   require 'open-uri'
 
   def index
+    url = "http://nightcity.en.cx/GameDetails.aspx?gid=43073"
+    doc = Nokogiri::HTML(open(url))
+    items = {}
+    @res = []
+    doc.css("#lnkPlayerInfo").each do |item|
+      newpath = 'http://nightcity.en.cx' + item.attr("href")
+      items[item.text] = newpath
+    end
+    items.each do |key,value|
+      link = Nokogiri::HTML(open(value))
+      sum = link.at_css("#enTeamDetailsPanel_divInfo div+span+div+span").text
+      @res.push(key + " have " + sum + "points")
+    end
   end
 
   def about
